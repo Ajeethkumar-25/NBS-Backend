47dae751900eaf46252674d42c666f7b378f986e
Ajeethkumar <ajkmr2525@gmail.com>
2025-04-21 17:55:11 +0530

    cors

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ app.mount("/static/horoscopes", StaticFiles(directory=horoscopes_dir), name="sta
 # CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173/", "http://localhost:3000/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
